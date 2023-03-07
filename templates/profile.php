1240de0eedcd5e1ca93950394239aae551ade7ab
Oleg <Seredenkogeo@gmail.com>
2023-03-07 10:50:04 +0300

    initial commit delete errors in profile

--- templates/profile.php
+++ templates/profile.php
@@ -17,9 +17,13 @@ if (!isset($_SESSION['user'])) {
                     <div class="col-md">
                         <form>
                             <div class="mb-4">
-                                <img src="<?= $_SESSION['user']['avatar'] ?>" class="rounded mx-auto d-block" width="250" alt="avatar">
-                                <div><h2><?= $_SESSION['user']['fullname'] ?> </h2></div>
-                                <div><a href="#" class="text-dark"><?= $_SESSION['user']['email'] ?></a></div>
+                                <img src="<? if (isset($_SESSION['user']['avatar'])) 
+                                { echo $_SESSION['user']['avatar']; }?>" 
+                                class="rounded mx-auto d-block" width="250" alt="avatar">
+                                <div><h2><? if (isset($_SESSION['user']['fullname'])) 
+                                { echo $_SESSION['user']['fullname']; }?> </h2></div>
+                                <div><a href="#" class="text-dark"><? if (isset($_SESSION['user']['email'])) 
+                                { echo $_SESSION['user']['email']; } ?></a></div>
                                 <div><a href="/profileEdit" class="text-dark">Редактировать профиль</a></div>
                                 <a href="/templates/logout.php" class="logout text-dark" >Выход</a>
                             </div>
